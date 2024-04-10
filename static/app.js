6a6f867fc1e4856ac62717508b76e838588d1e43
bshongwe <dru.da.beat.hustle@gmail.com>
2024-04-10 16:39:50 +0000

    Step 7: JS - Autocomplete widget script update

--- static/app.js
+++ static/app.js
@@ -61,22 +61,53 @@ const fetchAndRenderStores = async (center) => {
 // #3.3 Map: store to circle
 // Step 6 update - check Git Version
 const storeToCircle = (store, map, infowindow) => {
-  const [lng, lat] = store.geometry.coordinates;
-  const circle = new google.maps.Circle({
-    radius: 50,
-    strokeColor: "#579d42",
-    strokeOpacity: 0.8,
-    strokeWeight: 5,
-    center: { lat, lng },
-    map,
-  });
-  circle.addListener("click", () => {
-    infowindow.setContent(`${store.properties.business_name}<br />
+	const [lng, lat] = store.geometry.coordinates;
+	const circle = new google.maps.Circle({
+		radius: 50,
+		strokeColor: "#579d42",
+		strokeOpacity: 0.8,
+		strokeWeight: 5,
+		center: { lat, lng },
+		map,
+	});
+	circle.addListener("click", () => {
+		infowindow.setContent(`${store.properties.business_name}<br />
       ${store.properties.address_address}<br />
       Austin, TX ${store.properties.zip_code}`);
-    infowindow.setPosition({ lat, lng });
-    infowindow.setOptions({ pixelOffset: new google.maps.Size(0, -30) });
-    infowindow.open(map);
-  });
-  return circle;
+		infowindow.setPosition({ lat, lng });
+		infowindow.setOptions({ pixelOffset: new google.maps.Size(0, -30) });
+		infowindow.open(map);
+	});
+	return circle;
+};
+
+// Step 7: Autocomplet widget
+const initAutocompleteWidget = () => {
+	// Add search bar for auto-complete
+	// Build and add the search bar
+	const placesAutoCompleteCardElement = document.getElementById("pac-card");
+	const placesAutoCompleteInputElement = placesAutoCompleteCardElement.querySelector(
+		"input"
+	);
+	const options = {
+		types: ["address"],
+		componentRestrictions: { country: "us" },
+		map,
+	};
+	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(
+		placesAutoCompleteCardElement
+	);
+	// Make the search bar into a Places Autocomplete search bar and select
+	// which detail fields should be returned about the place that
+	// the user selects from the suggestions.
+	const autocomplete = new google.maps.places.Autocomplete(
+		placesAutoCompleteInputElement,
+		options
+	);
+	autocomplete.setFields(["address_components", "geometry", "name"]);
+	map.addListener("bounds_changed", () => {
+		autocomplete.setBounds(map.getBounds());
+	});
+
+	// TODO: Respond when a user selects an address
 };
