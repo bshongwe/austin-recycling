a6d434795a505ae6604f3dd68e6cab56acd29ef3
bshongwe <dru.da.beat.hustle@gmail.com>
2024-04-10 16:02:59 +0000

    Task 5 - JS: Map fetch

--- static/app.js
+++ static/app.js
@@ -13,12 +13,40 @@ const AUSTIN = { lat: 30.262129, lng: -97.7468 };
 
 // #2.1 Map: async
 async function initialize() {
-  initMap();
+	initMap();
 
-  // #2.1.1 TODO: Initialize an infoWindow
+	// #2.1.1 TODO: Initialize an infoWindow
 
-  // #2.1.2 Fetch and render stores as circles on map
-  fetchAndRenderStores(AUSTIN);
+	// #2.1.2 Fetch and render stores as circles on map
+	fetchAndRenderStores(AUSTIN);
 
-  // #2.1.3 TODO: Initialize the Autocomplete widget
+	// #2.1.3 TODO: Initialize the Autocomplete widget
 }
+
+// #3.1 Map: function
+const initMap = () => {
+	// #3.1.1 TODO: Start Distance Matrix service
+
+	// #3.1.2 The map, centered on Austin, TX
+	map = new google.maps.Map(document.querySelector("#map"), {
+		center: AUSTIN,
+		zoom: 14,
+		// #3.1.3 mapId: 'YOUR_MAP_ID_HERE',
+		clickableIcons: false,
+		fullscreenControl: false,
+		mapTypeControl: false,
+		rotateControl: true,
+		scaleControl: false,
+		streetViewControl: true,
+		zoomControl: true,
+	});
+};
+
+// #3.2 Map: fetch
+const fetchAndRenderStores = async (center) => {
+	// #3.2.1 Fetch the stores from the data source
+	stores = (await fetchStores(center)).features;
+
+	// #3.2.2 Create circular markers based on the stores
+	circles = stores.map((store) => storeToCircle(store, map));
+};
