527bdf2816f199fcd602dfc276ac2aaf1dcf2394
bshongwe <dru.da.beat.hustle@gmail.com>
2024-04-10 15:55:39 +0000

    Task 5 - JS: Map async function

--- static/app.js
+++ static/app.js
@@ -8,3 +8,17 @@ let stores = [];
 
 // #1.2 The location of Austin, TX
 const AUSTIN = { lat: 30.262129, lng: -97.7468 };
+
+// #2. Funtions
+
+// #2.1 Map: async
+async function initialize() {
+  initMap();
+
+  // #2.1.1 TODO: Initialize an infoWindow
+
+  // #2.1.2 Fetch and render stores as circles on map
+  fetchAndRenderStores(AUSTIN);
+
+  // #2.1.3 TODO: Initialize the Autocomplete widget
+}
